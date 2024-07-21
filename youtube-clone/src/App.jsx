3e1409514c25ce01bd0bc42acb65ba7ca730d5e6
Rift <hegide.dev@gmail.com>
2024-07-21 14:07:58 -0400

    add routes

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Video from "./Pages/Video/Video";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
     </div>
   );
 }
