dbb885d708289e4ad573bd8a768a9a86bcb08726
Rift <hegide.dev@gmail.com>
2024-07-21 15:36:57 -0400

    feed html

--- youtube-clone/src/Pages/Home/Home.jsx
+++ youtube-clone/src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Home.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import Feed from "../../Components/Feed/Feed";
 
 function Home({ sidebar }) {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   );
 }
